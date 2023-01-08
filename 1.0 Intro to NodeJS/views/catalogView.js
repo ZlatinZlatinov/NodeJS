8da51ce954f5e63542c0f38a55fa0622113feb32
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2023-01-08 22:39:22 +0200

    small updates

--- 1.0 Intro to NodeJS/views/catalogView.js
+++ 1.0 Intro to NodeJS/views/catalogView.js
@@ -1,7 +1,12 @@
-const { changeViews } = require("./htmlViews");
+const { changeViews, items } = require("./htmlViews");
 
 function catalogView(req, res) {
-    const catalogView = `<h1>Catalog</h1> <h3>List of Items:</h3>`;
+    const catalogView = `
+    <h1>Catalog</h1> 
+    <h3>List of Items:</h3>
+    <ul>
+    ${items.map((i) => `<li>${i.name} - ${i.color}</li>`).join('\n')}
+    </ul>`;
     res.write(changeViews(catalogView, 'Catalog'));
     res.end();
 } 
--- 1.0 Intro to NodeJS/views/htmlViews.js
+++ 1.0 Intro to NodeJS/views/htmlViews.js
@@ -1,4 +1,4 @@
-function changeViews(content, title){
+function changeViews(content, title) {
     return `
     <!DOCTYPE html>
 <html lang="en">
@@ -20,8 +20,28 @@ function changeViews(content, title){
 </body>
 </html>
     `;
-} 
+}
+
+const items = [
+    {
+        id: 'wasd0001',
+        name: 'item1',
+        color: 'Red'
+    }, 
+    {
+        id: 'wasd0002',
+        name: 'item2',
+        color: 'Green'
+    }, 
+    {
+        id: 'wasd0003',
+        name: 'item3',
+        color: 'Blue'
+    }
+
+];
 
 module.exports = {
-    changeViews
+    changeViews, 
+    items
 }
