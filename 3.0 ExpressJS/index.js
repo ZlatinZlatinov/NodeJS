ee3213070b7719e9daa5ab852998ea7a3283d47e
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2023-01-18 00:42:22 +0200

    trugna nai posle toz express server we

--- 3.0 ExpressJS/index.js
+++ 3.0 ExpressJS/index.js
@@ -1,11 +1,18 @@
 const express = require('express'); 
-const hbr = require('express-handlebars');
+const hbs = require('express-handlebars');
 
-
-const handlebars = hbr.create({
-    extname:'.hbs'
-}); 
+const handlebars = hbs.create({
+    extname: '.hbs'
+});
 
 const app = express(); 
 
 app.engine('.hbs', handlebars.engine); 
+app.set('view engine', '.hbs'); 
+
+app.get('/', (req, res) =>{
+    res.render('home.hbs');
+});
+
+app.listen(3000); 
+console.log('The server is now running on port 3000...');
