dc93e4b289e2b1fc038b73955936eb8759151fa5
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2023-01-18 00:58:54 +0200

    just creating more stuff

--- 3.0 ExpressJS/index.js
+++ 3.0 ExpressJS/index.js
@@ -1,18 +1,20 @@
-const express = require('express'); 
-const hbs = require('express-handlebars');
+const express = require('express');
+const homeController = require('./viewControllers/homeController');
+const aboutController = require('./viewControllers/about.Controller');
+
+const hbs = require('express-handlebars'); // from here
 
 const handlebars = hbs.create({
     extname: '.hbs'
 });
 
-const app = express(); 
+const app = express();
 
-app.engine('.hbs', handlebars.engine); 
-app.set('view engine', '.hbs'); 
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs'); //to here is boiler plate
 
-app.get('/', (req, res) =>{
-    res.render('home.hbs');
-});
+app.get('/', homeController);  
+app.get('/about', aboutController);
 
-app.listen(3000); 
+app.listen(3000);
 console.log('The server is now running on port 3000...');
