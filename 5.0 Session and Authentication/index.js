585d3c976589921b5599ef3a04d2a46517b1d0be
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2023-02-02 21:29:20 +0200

    testing express session

---
+++ 5.0 Session and Authentication/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const session = require('express-session');
+
+const server = express();
+
+server.use(session({
+    secret: 'my-secret-key',
+    resave: 'false',
+    /*saveUnitialized: true,*/
+    cookie: { secure: false }
+}));
+
+server.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1>');
+})
+
+server.get('/setSession', (req, res) => {
+    req.session.message = 'Hello';
+    res.end('Session set');
+});
+
+server.get('/readSession', (req, res) => {
+    res.json(req.session); // returns the session in json format
+});
+
+server.listen(3000, () => {
+    console.log('The server is now listening on port 3000...');
+});
