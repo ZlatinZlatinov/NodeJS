a1aa8e2002de9f92387dc1a0e331f442f42d2c6f
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2023-02-02 21:38:15 +0200

    playing with bcrypt

---
+++ 5.0 Session and Authentication/hash.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+const myRawPAssword = '1234wasd';
+
+bcrypt.genSalt(saltRounds, (err, salt) => {
+    bcrypt.hash(myRawPAssword, salt, (err, hash) => {
+        console.log('The hashed passsword is: ' + hash);
+    });
+});
+
+const keyboardHeadSmash = '$2b$10$Hu/u5SZNLyMqB4OmLCElj.zljIvNlxVvIHc0ASHPt8jznb6l6OMS.';
+
+bcrypt.compare(myRawPAssword, keyboardHeadSmash, (err, res) => {
+    console.log(res);
+});
