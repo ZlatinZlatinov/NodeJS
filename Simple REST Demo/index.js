2935927166d0a826630a64b0cceafe1a7c279297
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2023-05-21 22:45:22 +0300

    basic rest service demo

--- Simple REST Demo/index.js
+++ Simple REST Demo/index.js
@@ -1,12 +1,66 @@
 const express = require('express');
-const cors = require('cors');
+//const cors = require('cors');
 
 const app = express();
-
-app.use(cors());
+//app.use(express.urlencoded({ extended: true })); -body parser
 app.use(express.static('static'));
+app.use(express.json());
+//app.use(cors());
 
 
+const items = [
+    {
+        id: 1,
+        name: 'Item 1',
+        price: 10
+    },
+    {
+        id: 2,
+        name: 'Item 2',
+        price: 20
+    },
+    {
+        id: 3,
+        name: 'Item 3',
+        price: 30
+    }
+];
+let id = 4;
+
+app.get('/data', (req, res) => {
+    //console.log('Request made!');
+    res.json(items);
+});
+
+app.post('/data', (req, res) => {
+    const name = req.body.name;
+    const price = Number(req.body.price);
+    const newItem = { name, price, id };
+    items.push(newItem);
+    id++;
+    res.status(201).json(newItem);
+});
+
+app.delete('/data/:id', (req, res) => {
+    const id = req.params.id;
+    const itemIndex = items.findIndex(x => x.id == id);
+    items.splice(itemIndex, 1);
+    res.status(202).end();
+}); 
+
+app.get('/data/:id', (req, res) => {
+    const id = req.params.id;
+    const item = items.find(x => x.id == id);
+    res.status(200).json(item);
+}); 
+
+app.put('/data/:id', (req, res) => {
+    const id = req.params.id;
+    const item = items.find(x => x.id == id);
+    item.name = req.body.name; 
+    item.price = Number(req.body.price);
+    res.status(202).end();
+});
 
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
--- Simple REST Demo/static/app.js
+++ Simple REST Demo/static/app.js
@@ -1,1 +1,113 @@
 console.log('Hello!');
+
+document.getElementById('load').addEventListener('click', loadItems);
+const ul = document.querySelector('ul.listItems');
+document.querySelector('form.createForm').addEventListener('submit', createItem);
+
+let editMode = false;
+let currID = null;
+
+async function loadItems() {
+    ul.textContent = '';
+    const response = await fetch('http://localhost:3000/data');
+
+    if (response.ok) {
+        const items = await response.json();
+
+        for (let i of items) {
+            fillList(i);
+        }
+    }
+}
+
+async function createItem(e) {
+    e.preventDefault();
+    const itemName = document.getElementById('itemName').value;
+    const itemPrice = document.getElementById('itemPrice').value;
+
+    if (editMode) {
+        const response1 = await fetch(`http://localhost:3000/data/${currID}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name: itemName, price: itemPrice, id: currID })
+        });
+
+        if (response1.ok) {
+            loadItems();
+            document.getElementById('itemName').value = '';
+            document.getElementById('itemPrice').value = '';
+            editMode = false;
+            currID = null;
+            return;
+        }
+    }
+
+    const response = await fetch('http://localhost:3000/data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: itemName, price: itemPrice })
+    });
+
+
+    if (response.status == 201) {
+        const newItem = await response.json();
+        fillList(newItem);
+        itemName.value = '';
+        itemPrice.value = '';
+    } else {
+        itemName.value = 'Whoops, sorry, something went bad xdd';
+    }
+}
+
+function fillList(i) {
+    const li = document.createElement('li');
+    li.textContent = `Name: ${i.name} - Price: ${(i.price).toFixed(2)}$`;
+    li.id = i.id;
+
+    const editBtn = createNewBtn('Edit', 'editBtn');
+    editBtn.addEventListener('click', editItem)
+    const deleteBtn = createNewBtn('Delete', 'deleteBtn');
+    deleteBtn.addEventListener('click', deleteItem);
+
+    li.appendChild(editBtn);
+    li.appendChild(deleteBtn);
+    ul.appendChild(li);
+}
+
+function createNewBtn(name, className) {
+    const btn = document.createElement('button');
+    btn.className = className;
+    btn.textContent = name;
+    return btn;
+}
+
+async function editItem(e) {
+    const li = e.target.parentNode;
+    currID = li.id;
+    const response = await fetch(`http://localhost:3000/data/${currID}`);
+
+    if (response.status == 200) {
+        const item = await response.json();
+        document.getElementById('itemName').value = item.name;
+        document.getElementById('itemPrice').value = item.price;
+    }
+
+    editMode = true;
+}
+
+async function deleteItem(e) {
+    const li = e.target.parentNode;
+    const id = li.id;
+
+    const response = await fetch(`http://localhost:3000/data/${id}`, {
+        method: 'DELETE',
+    });
+
+    if (response.ok) {
+        li.remove();
+    }
+}
